998767fbde5e99c0c9c53d078ca7f2658d3d9ab4
Maxi Ferreira <charca@gmail.com>
2016-05-04 20:13:26 -0700

    Added Privacy Policy and Terms of Service

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Yostik' });
 });
 
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', { title: 'Yostik | Privacy Policy' });
+});
+
+router.get('/tos', function(req, res, next) {
+  res.render('tos', { title: 'Yostik | Terms of Service' });
+});
+
 module.exports = router;
