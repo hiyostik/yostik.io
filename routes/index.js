4417ff878eecd1d01da5cc3985088118b1de804c
Maxi Ferreira <charca@gmail.com>
2016-08-04 01:30:25 -0700

    Added Pokenum privacy and tos

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,39 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/privacy', function(req, res, next) {
-  res.render('privacy', { title: 'Yostik | Privacy Policy' });
+  res.render('privacy', {
+    title: 'Yostik | Privacy Policy',
+    product: 'Yostik',
+    website: 'www.yostik.io',
+    email: 'hi@yostik.io'
+  });
 });
 
 router.get('/tos', function(req, res, next) {
-  res.render('tos', { title: 'Yostik | Terms of Service' });
+  res.render('tos', {
+    title: 'Yostik | Terms of Service',
+    product: 'Yostik',
+    website: 'www.yostik.io',
+    email: 'hi@yostik.io'
+  });
+});
+
+router.get('/pokenum/privacy', function(req, res, next) {
+  res.render('privacy', {
+    title: 'PokéNum | Privacy Policy',
+    product: 'PokéNum',
+    website: 'www.yostik.io/pokenum',
+    email: 'charca@gmail.com'
+  });
+});
+
+router.get('/pokenum/tos', function(req, res, next) {
+  res.render('tos', {
+    title: 'PokéNum | Terms of Service',
+    product: 'PokéNum',
+    website: 'www.yostik.io/pokenum',
+    email: 'charca@gmail.com'
+  });
 });
 
 module.exports = router;
